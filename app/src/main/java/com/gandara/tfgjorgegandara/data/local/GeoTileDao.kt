72fd8d77061179d9977d6af543428a0edbe9d0e1
Wachu248 <98882488+Wachu248@users.noreply.github.com>
2026-06-01 10:57:58 +0200

    Pantalla de Ajustes. Botón de explicacion de ponderaciones. Opcion de buffersize=1024. Limpieza de paquetes vacios.

--- app/src/main/java/com/gandara/tfgjorgegandara/data/local/GeoTileDao.kt
+++ app/src/main/java/com/gandara/tfgjorgegandara/data/local/GeoTileDao.kt
@@ -16,20 +16,4 @@ interface GeoTileDao {
     """)
     suspend fun upsertSampleToTile(tileId: String, timeBucket: Long, dbValue: Double)
 
-    @Query("SELECT * FROM geo_tiles WHERE timeBucket >= :sinceBucket")
-    suspend fun getRecentTiles(sinceBucket: Long): List<GeoTile>
-
-    @Query("""
-        SELECT * FROM geo_tiles 
-        WHERE timeBucket >= :sinceBucket
-        AND CAST(SUBSTR(tileId, 1, INSTR(tileId, '_') - 1) AS INTEGER) BETWEEN :minLatGrid AND :maxLatGrid
-        AND CAST(SUBSTR(tileId, INSTR(tileId, '_') + 1) AS INTEGER) BETWEEN :minLonGrid AND :maxLonGrid
-    """)
-    suspend fun getTilesInBounds(
-        sinceBucket: Long,
-        minLatGrid: Int,
-        maxLatGrid: Int,
-        minLonGrid: Int,
-        maxLonGrid: Int
-    ): List<GeoTile>
 }
